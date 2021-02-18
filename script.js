11ff17147e97be0821e43e3d1de3dab7bfad9073
Ananyacodes <69697046+Ananyacodes@users.noreply.github.com>
2021-02-18 16:11:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+document.addEventListener('keydown', (event) => {
+  console.log(event.keyCode);
+  var key = event.keyCode;
+  if(key == 79) {
+  	var audio = new Audio("Josh Gad - When I Am Older (From Frozen 2 ).mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('79').style.backgroundColor = "aqua";
+  	document.getElementById('79').style.boxShadow = "0px 0px 20px 0px aqua";
+  	setTimeout(function(){
+  		document.getElementById('79').style.backgroundColor = "transparent";
+  		document.getElementById('79').style.boxShadow = "none";
+  	}, 500)
+  }
+  if(key == 83) {
+  	var audio = new Audio("Jonathan Groff - Lost in the Woods (From Frozen 2 ).mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('83').style.backgroundColor = "aqua";
+  	document.getElementById('83').style.boxShadow = "0px 0px 20px 0px aqua";
+  	setTimeout(function(){
+  		document.getElementById('83').style.backgroundColor = "transparent";
+  		document.getElementById('83').style.boxShadow = "none";
+  	}, 500)
+  }
+   if(key == 89) {
+  	var audio = new Audio("Disney_Frozen_2_Intro-637845.mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('89').style.backgroundColor = "aqua";
+  	document.getElementById('89').style.boxShadow = "0px 0px 20px 0px aqua";
+  	setTimeout(function(){
+  		document.getElementById('89').style.backgroundColor = "transparent";
+  		document.getElementById('89').style.boxShadow = "none";
+  	}, 500)
+  } 
+  if(key == 65) {
+  	var audio = new Audio("Kristen Bell - The Next Right Thing (From Frozen 2 ).mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('65').style.backgroundColor = "purple";
+  	document.getElementById('65').style.boxShadow = "0px 0px 20px 0px aqua";
+  	setTimeout(function(){
+  		document.getElementById('65').style.backgroundColor = "transparent";
+  		document.getElementById('65').style.boxShadow = "none";
+  	}, 500)
+  } 
+  if(key == 69) {
+  	var audio = new Audio("Idina Menzel, AURORA - Into the Unknown (From Frozen 2 ).mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('69').style.backgroundColor = "purple";
+  	document.getElementById('69').style.boxShadow = "0px 0px 20px 0px purple";
+  	  	setTimeout(function(){
+  		document.getElementById('69').style.backgroundColor = "transparent";
+  		document.getElementById('69').style.boxShadow = "none";
+  	}, 500)
+  }
+   if(key == 75) {
+  	var audio = new Audio("Some Things Never Change (From Frozen 2 ).mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('75').style.backgroundColor = "purple";
+  	document.getElementById('75').style.boxShadow = "0px 0px 20px 0px purple";
+  	setTimeout(function(){
+  		document.getElementById('75').style.backgroundColor = "transparent";
+  		document.getElementById('75').style.boxShadow = "none";
+  	}, 500)
+  }
+  	 if(key == 73) {
+  	var audio = new Audio("Evan Rachel Wood - All Is Found (From Frozen 2 ).mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('73').style.backgroundColor = "purple";
+  	document.getElementById('73').style.boxShadow = "0px 0px 20px 0px purple";
+  	setTimeout(function(){
+  		document.getElementById('73').style.backgroundColor = "transparent";
+  		document.getElementById('73').style.boxShadow = "none";
+  	}, 500)
+  }
+   if(key == 78 ) {
+  	var audio = new Audio("Idina Menzel, Evan Rachel Wood - Show Yourself (From Frozen 2 ).mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('78').style.backgroundColor = "white";
+  	document.getElementById('78').style.boxShadow = "0px 0px 20px 0px white";
+  	setTimeout(function(){
+  		document.getElementById('78').style.backgroundColor = "transparent";
+  		document.getElementById('78').style.boxShadow = "none";
+  	}, 500)
+  }
+   if(key == 72) {
+  	var audio = new Audio("Kristen Bell - Love Is An Open Door.mp3");
+  	audio.currentTime = 0;
+  	audio.play();
+  	document.getElementById('72').style.backgroundColor = "white";
+  	document.getElementById('72').style.boxShadow = "0px 0px 20px 0px white";
+  	setTimeout(function(){
+  		document.getElementById('72').style.backgroundColor = "transparent";
+  		document.getElementById('72').style.boxShadow = "none";
+  	}, 500)
+  }
+});
+	
